05d3a2c50692216db143fd3db425c7d3d9d95813
nachitoxx6262 <ignachio2003@hotmail.com>
2024-09-24 12:12:13 -0300

    environments var

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-# URL de conexión a PostgreSQL
-DATABASE_URL = "postgresql://postgres:admin@localhost/proyecto1"
+
+DATABASE_URL = os.getenv('DATABASE')
+
 # Crear el motor de conexión
 engine = create_engine(DATABASE_URL)
 # Crear una clase Base para los modelos
@@ -13,7 +17,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Crear las tablas en la base de datos
 try:
     Base.metadata.create_all(bind=engine)
-    print("Tablas creadas correctamente.")
 except Exception as e:
     print(f"Error creando las tablas: {e}")
 # Dependencia para obtener la sesión de base de datos
